14ca27cd0fb29aec244f01c953e1905c6f62ee63
TheMatth07 <mateuscorreia022@gmail.com>
2025-04-06 21:21:36 +0000

    adicionando aula5

---
+++ aula5/target/debug/deps/aula5-9aae64bdee9f44b9.d
@@ -0,0 +1,5 @@
+/workspaces/Mateus.Correia.ADS3NB/aula5/target/debug/deps/aula5-9aae64bdee9f44b9: src/main.rs
+
+/workspaces/Mateus.Correia.ADS3NB/aula5/target/debug/deps/aula5-9aae64bdee9f44b9.d: src/main.rs
+
+src/main.rs:
